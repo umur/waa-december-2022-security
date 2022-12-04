0e4010f5fcc6acf0db159d4756a6105e2018f7b6
deStarxis <humagainuzjwol@gmail.com>
2022-12-04 15:53:06 -0600

    initialized project

---
+++ spring-security-assignment/src/main/java/com/example/springsecurityassignment/SpringSecurityAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAssignmentApplication.class, args);
+    }
+
+}
---
+++ spring-security-assignment/src/test/java/com/example/springsecurityassignment/SpringSecurityAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
