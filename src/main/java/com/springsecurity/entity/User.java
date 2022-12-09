ef52079193aabc1f23b6aa072d17569241400071
sandesh56 <sandeshrauth644@gmail.com>
2022-12-08 20:55:55 -0600

    Feat: entity defined for user

---
+++ src/main/java/com/springsecurity/entity/User.java
@@ -0,0 +1,23 @@
+package com.springsecurity.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String email;
+    private String password;
+    private String firstname;
+    private String lastname;
+
+}
