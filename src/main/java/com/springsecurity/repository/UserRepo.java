af6df5b7dbe67f1a5afde620f1a66851176eb16f
sandesh56 <sandeshrauth644@gmail.com>
2022-12-08 23:32:56 -0600

    Feat: service implemented for product and user authentication

---
+++ src/main/java/com/springsecurity/repository/UserRepo.java
@@ -0,0 +1,10 @@
+package com.springsecurity.repository;
+
+import com.springsecurity.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends CrudRepository<User, Integer> {
+    User findByEmail(String email);
+}
